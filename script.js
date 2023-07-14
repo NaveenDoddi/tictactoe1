5606cc7670a5cc8f31afaf389466ced85c779a04
naveendoddi <prasadnaveen847@gmail.com>
2023-07-14 23:59:56 +0530

    jfkl

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var count = Math.floor(Math.random() * 2)
 // var count = 0
 var count1 = 0
-var arr =[0,1,2,3,4,5,6,7,8]
+var arr = [0,1,2,3,4,5,6,7,8]
 
 function handleClick(clickedElement) {
     
@@ -16,7 +16,7 @@ function handleClick(clickedElement) {
     console.log(arr)
     
 }
-  
+var arr1 = [[1,2,3,0,0],[4,5,6,0,0],[7,8,9,0,0],[1,4,7,0,0],[2,5,8,0,0],[3,6,9,0,0],[1,5,9,0,0],[3,5,7,0,0]];
 
 
 function run(){
@@ -37,7 +37,7 @@ function run(){
     var redarr = [];
     var blackarr = [];
     
-    for(let i = 1; i < 10; i++){
+    for(let i = 0; i < 9; i++){
         var color = document.getElementById(i).style.backgroundColor;
         var id = document.getElementById(i).id;
         if(color == "red"){
@@ -46,6 +46,61 @@ function run(){
             blackarr.push(id);
         }
         
+    }
+    for(let i = 0; i < 8; i++){
+        var redcount = 0;
+        for(let j = 0; j < 5; j++){
+            for(let x = 0; x<redarr.length; x++){
+                if(arr1[i][j] == redarr[x]){
+                    redcount++;
+
+                }
+                if(redcount == 3){
+                    
+                    for(let y = 0 ; y<3; y++){
+                        document.getElementById(arr[i][y]).className = "yes"
+                    }
+                    
+                    document.getElementById("result").innerText = "Red-Winner";
+                    document.getElementById("result").style.color = "red"
+                    setTimeout(() => {
+                        window.location.reload();
+                    }, 3000);
+                }
+            }
+        
+            
+        }
+
+        var blackcount = 0;
+        for(let j = 0; j < 5; j++){
+            for(let x =0; x < blackarr.length; x++){
+                if(arr[i][j] == blackarr[x]){
+                    blackcount++;
+                }
+                if(blackcount == 3){
+                    for(let y = 0 ; y<3; y++){
+                        document.getElementById(arr[i][y]).id = "yes"
+                    }
+
+                    document.getElementById("result").innerText = "Black-Winner";
+                    document.getElementById("result").style.color = "balck"
+
+                    setTimeout(() => {
+                        window.location.reload();
+                    }, 3000);
+                }
+                
+            }
+        }
+
+
+    }
+    if(count1 == 9){
+        document.getElementById("result").innerText = "Try Again!!!";
+        setTimeout(() => {
+            window.location.reload();
+        }, 3000);
     }
     
 }
